091e69b34c9364f6e500fdc5bf31785aa2a52963
joshmoy <obodaij@gmail.com>
2026-02-01 19:00:40 +0000

    feature: add fonts and @tailwindcss/vit

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ export function App() {
       <a href="https://preactjs.com" target="_blank">
         <img src={preactLogo} alt="Preact logo" height="160" width="160" />
       </a>
-      <h1>Get Started building Vite-powered Preact Apps </h1>
+      <h1 className="font-heading">Get Started building Vite-powered Preact Apps </h1>
       <section>
         <Resource
           title="Learn Preact"
@@ -40,7 +40,7 @@ function Resource(props) {
 	return (
 		<a href={props.href} target="_blank" class="resource">
 			<h2>{props.title}</h2>
-			<p>{props.description}</p>
+			<p className="font-body">{props.description}</p>
 		</a>
 	);
 }
